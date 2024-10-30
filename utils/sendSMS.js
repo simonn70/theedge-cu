771f71f8668f773c6c3b1c868c49d409a3c7ee29
simonn70 <simonadjei70@gmail.com>
2024-10-30 15:47:58 +0000

    update stuff

--- utils/sendSMS.js
+++ utils/sendSMS.js
@@ -3,8 +3,11 @@ const axios = require("axios");
 const sendSMS = async (contactNumber, message) => {
   // const apiKey = "nHBzjRiacu090IFfVpZdugx1g";
   // const senderId = "Kan Credit";
-   const apiKey = "bIOb9eHhApWR3mw4PzPU6k8a1";
-  const senderId = "The Edge CU";
+  //  const apiKey = "bIOb9eHhApWR3mw4PzPU6k8a1";
+  // const senderId = "The Edge CU";
+
+    const apiKey = "Cc5wOuXSTjwduc8lCnvqdKgYp";
+  const senderId = "GNTDA";
 
   const mNotifyUrl = `https://apps.mnotify.net/smsapi?key=${apiKey}&to=${contactNumber}&msg=${encodeURIComponent(
     message
